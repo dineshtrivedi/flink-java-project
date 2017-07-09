28c94038f2010ac330d99914ec764f0691e69430
Dinesh Trivedi <dinesh.dart@gmail.com>
2017-07-09 10:02:11 -0700

    Starting unit testing

--- src/main/java/com/training/data/artisans/taxi/TaxiRideCleansingRunner.java
+++ src/main/java/com/training/data/artisans/taxi/TaxiRideCleansingRunner.java
@@ -1,11 +1,22 @@
 package com.training.data.artisans.taxi;
 
+import com.dataartisans.flinktraining.exercises.datastream_java.datatypes.TaxiRide;
+import com.dataartisans.flinktraining.exercises.datastream_java.sources.TaxiRideSource;
 import org.apache.flink.streaming.api.TimeCharacteristic;
+import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 
 public class TaxiRideCleansingRunner {
 
+	// events are out of order by max 60 seconds
+	private static final int MAX_EVENT_DELAY_DEFAULT = 60;
+
+	// events of 10 minutes are served in 1 second (10 * 60) = 600s
+	private static final int SERVING_SPEED_FACTOR_DEFAULT = 600;
+
 	public static void main(String[] args) throws Exception {
+
+		final String dataFilePath = "/home/dinesh/workspace/flink-java-project/nycTaxiRides.gz";
 
 		// get an ExecutionEnvironment
 		StreamExecutionEnvironment env =
@@ -13,10 +24,15 @@ public class TaxiRideCleansingRunner {
 		// configure event-time processing
 		env.setStreamTimeCharacteristic(TimeCharacteristic.EventTime);
 
-		final String dataFilePath = "/home/dinesh/workspace/flink-java-project/nycTaxiRides.gz";
+		// get the taxi ride data stream
+		DataStream<TaxiRide> rides = env.addSource(
+				new TaxiRideSource(dataFilePath, MAX_EVENT_DELAY_DEFAULT, SERVING_SPEED_FACTOR_DEFAULT));
 
-		TaxiRideCleansing taxiRideCleansing = new TaxiRideCleansing(env, dataFilePath, 0, 0);
-		taxiRideCleansing.execute();
+		TaxiRideCleansing taxiRideCleansing = new TaxiRideCleansing(0, 0);
+		DataStream<TaxiRide> filteredTaxis = taxiRideCleansing.execute(rides);
+
+		filteredTaxis.print();
+		env.execute("Running Taxi Ride Cleansing");
 	}
 
 }
---
+++ src/test/java/com/training/data/artisans/taxi/TaxiRideCleansingTest.java
@@ -0,0 +1,13 @@
+package com.training.data.artisans.taxi;
+
+
+import org.apache.flink.streaming.util.StreamingMultipleProgramsTestBase;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TaxiRideCleansingTest extends StreamingMultipleProgramsTestBase {
+	@Test
+	public void dummyTest(){
+		Assert.assertTrue(true);
+	}
+}
