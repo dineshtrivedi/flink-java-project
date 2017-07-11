5929c5963a0f66157ab1b1dd1263a60e797e2f3b
Dinesh Trivedi <dinesh.dart@gmail.com>
2017-07-11 13:37:39 -0700

    travis fixins 2

--- ci/install-flink-training-exercises.sh
+++ ci/install-flink-training-exercises.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 if [ ! -d "~/flink-training-exercises" ]; then
-  git clone https://github.com/dataArtisans/flink-training-exercises.git ~/
+  git clone https://github.com/dataArtisans/flink-training-exercises.git ~/flink-training-exercises
 fi
 
 cd ~/flink-training-exercises
--- ci/run_codacy_report.sh
+++ ci/run_codacy_report.sh
@@ -2,6 +2,6 @@
 set -e  # If occur any error, exit
 set -x  # Verbose mode
 
-. ci/env.sh
+. ci/codacy_credential_key.sh
 
 java -cp ~/.codacy/repoerter/codacy-coverage-reporter-assembly-latest.jar com.codacy.CodacyCoverageReporter -l Java -r target/site/jacoco-ut/jacoco.xml
